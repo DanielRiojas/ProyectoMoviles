0f25b64e71a0070c5ba832d5c3fe5a7d00f4edb4
Manuel Wereb <manuel.wereb>
2021-10-25 12:19:57 -0500

    Updates

--- ProyectoTEC/ViewControllerRegistrate.swift
+++ ProyectoTEC/ViewControllerRegistrate.swift
@@ -9,11 +9,9 @@ import UIKit
 
 class ViewControllerRegistrate: UIViewController {
     
-    
-    
     @IBOutlet weak var registrate: UIButton! // Button
     
-    @IBOutlet weak var tf_correo: UITextField! // Textfield
+    @IBOutlet weak var tf_correo: BindingTextField! // Textfield
     @IBOutlet weak var tf_contrasena: UITextField!
     @IBOutlet weak var tf_repiteContrasena: UITextField!
     @IBOutlet weak var tf_nombre: UITextField!
@@ -21,11 +19,19 @@ class ViewControllerRegistrate: UIViewController {
     @IBOutlet weak var tf_apellidoMaterno: UITextField!
     @IBOutlet weak var tf_fechaNacimiento: UITextField!
     
+    var emailIsValid = false
+    var datePicker: UIDatePicker?
+    var pickerToolbar: UIToolbar?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Setup email validation
+        emailValidation()
+        
+        // Textfield with date
+        dateTextfield()
         
         // Button Configuration
         registrate.layer.cornerRadius = 25
@@ -46,6 +52,45 @@ class ViewControllerRegistrate: UIViewController {
         view.endEditing(true)
     }
     
+    // MARK: - Textfield with date
+    func dateTextfield() {
+        pickerToolbar = UIToolbar()
+        pickerToolbar?.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneBtnClicked(_:)))
+        pickerToolbar?.setItems([doneButton], animated: true)
+        tf_fechaNacimiento.inputAccessoryView = pickerToolbar
+        
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.frame = CGRect(x: 10, y: 50, width: self.view.frame.width, height: 100)
+        tf_fechaNacimiento.inputView = datePicker
+    }
+        
+    @objc func doneBtnClicked(_ button: UIBarButtonItem?) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        tf_fechaNacimiento.text = dateFormatter.string(from: datePicker!.date)
+        view.endEditing(true)
+        }
+    
+    
+    // MARK: - Email validation
+    func emailValidation() {
+        tf_correo.bind { [weak self] (text) in if let isValid = self?.isValidEmail(text) {
+                if (isValid == true) {
+                    self!.emailIsValid = true
+                }
+            }
+        }
+    }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
 
     
     // MARK: - Prepare for segue
@@ -58,13 +103,25 @@ class ViewControllerRegistrate: UIViewController {
             present(alerta, animated: true, completion: nil)
             return false
         }
+        else if (emailIsValid == false) {
+            let alerta = UIAlertController(title: "Error", message: "Correo electrónico no es valido", preferredStyle: .alert)
+            let accion = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alerta.addAction(accion)
+            present(alerta, animated: true, completion: nil)
+            return false
+        }
+        else if (tf_contrasena.text! != tf_repiteContrasena.text!) {
+            let alerta = UIAlertController(title: "Error", message: "Las contraseñas no coinciden", preferredStyle: .alert)
+            let accion = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alerta.addAction(accion)
+            present(alerta, animated: true, completion: nil)
+            return false
+        }
         else {
             return true
         }
     }
 
-
-    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (sender as! UIButton) == registrate {
