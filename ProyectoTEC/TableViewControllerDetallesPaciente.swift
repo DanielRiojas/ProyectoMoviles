25f52d55c44045c64eb9104b91a51335cbaebd5f
iam1997 <manuel.wereb@outlook.de>
2021-11-08 16:00:28 -0800

    Agregué la vista con detalles sobre el paciente

---
+++ ProyectoTEC/TableViewControllerDetallesPaciente.swift
@@ -0,0 +1,32 @@
+//
+//  TableViewControllerDetallesPaciente.swift
+//  ProyectoTEC
+//
+//  Created by user191778 on 11/8/21.
+//
+
+import UIKit
+
+class TableViewControllerDetallesPaciente: UITableViewController {
+
+    @IBOutlet weak var lbl_nombrePaciente: UILabel!
+    @IBOutlet weak var lbl_nombre: UILabel!
+    @IBOutlet weak var lbl_apellidoPaterno: UILabel!
+    @IBOutlet weak var lbl_apellidoMaterno: UILabel!
+    @IBOutlet weak var lbl_fechaNacimiento: UILabel!
+    @IBOutlet weak var lbl_sexo: UILabel!
+    @IBOutlet weak var lbl_numeroTelefono: UILabel!
+    @IBOutlet weak var lbl_ubicacion: UILabel!
+    
+    @IBOutlet weak var img_fotoPaciente: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    
+
+}
---
+++ ProyectoTEC/ViewControllerDoctorPaciente.swift
@@ -0,0 +1,31 @@
+//
+//  ViewControllerDoctorTodosPacientes.swift
+//  ProyectoTEC
+//
+//  Created by user191778 on 11/8/21.
+//
+
+import UIKit
+
+class ViewControllerDoctorPacientes: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
